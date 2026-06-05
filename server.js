8dd2ba844acf97fd59feebd5d28b9ed2944b8b3c
aliatwood <aliatwood0104@gmail.com>
2026-06-05 20:00:51 +0200

    Update server.js
    Kopplade in menuRoutes i server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const menuRoutes = require("./routes/menuRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/menu", menuRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "API fungerar" });
